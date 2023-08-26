9ec8de1708d3a1b4a9a2afddb4f2be3c5c397931
sdj3701 <110330701+sdj3701@users.noreply.github.com>
2023-08-26 19:49:07 +0900

    hongdata
    swap

---
+++ hongData/hongData/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hongData
+//
+//  Created by 신동주 on 2023/08/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
