153b8e273466ae9d8856a46afc3de038bc935d64
sdj3701 <110330701+sdj3701@users.noreply.github.com>
2023-09-04 19:44:29 +0900

    asd

--- hongData/hongData/hongData/InsertionSort.cpp
+++ hongData/hongData/hongData/InsertionSort.cpp
@@ -3,10 +3,34 @@
 using namespace std;
 
 void Print(int* arr, int n);
-void Swap(int& a, int b);
+void Swap(int& a, int& b);
 
 int main()
 {
+	{
+		int arr[] = { 6,5,4,3,2,1 };
+		int n = sizeof(arr) / sizeof(arr[0]);
+
+		Print(arr, n);
+
+		int i = 4;
+
+		for (int i = 1; i < n;i++)
+		{
+			int key = arr[i];
+			int j = i;
+			for (;j > 0 && arr[j - 1] > key;j--)
+			{
+				arr[j] = arr[j - 1];
+				Print(arr, n);
+			}
+			arr[j] = key;
+		}
+
+		Print(arr, n);
+		cout << endl;
+
+	}
 
 }
 
@@ -25,3 +49,16 @@ void Swap(int& a, int& b)
 	a = b;
 	b = temp;
 }
+
+
+#pragma region 노트
+/*
+
+	삽입 정렬
+	값을 저장하는 것
+	중간에 for문 탈출
+
+
+
+*/
+#pragma endregion
