c98c39b9743f2003abea2817c49e18d8ac9b0f33
sdj3701 <110330701+sdj3701@users.noreply.github.com>
2023-10-17 20:17:34 +0900

    모르겠다

--- hongData/HongLabDataStructures-main/Ex0306_SparseMatrix/SparseMatrix.cpp
+++ hongData/HongLabDataStructures-main/Ex0306_SparseMatrix/SparseMatrix.cpp
@@ -16,6 +16,7 @@ SparseMatrix::SparseMatrix(int num_rows, int num_cols, int capacity)
 	num_terms_ = 0;
 
 	terms_ = new MatrixTerm[capacity];
+
 }
 
 // 복사 생성자 (b를 복사)
@@ -28,7 +29,7 @@ SparseMatrix::SparseMatrix(const SparseMatrix& b)
 	capacity_ = b.capacity_;
 	num_terms_ = b.num_terms_;
 
-	memcpy(num_terms_[capacity_], b.num_terms_[capacity_], capacity_);
+	memcpy(terms_, b.terms_, sizeof(MatrixTerm) * num_terms_);
 
 }
 
@@ -44,7 +45,12 @@ void SparseMatrix::SetValue(int row, int col, float value)
 	if (value == 0.0f) return; // value가 0이 아닌 term만 저장
 
 	// TODO:
-	terms_[row * num_rows_ + col].value = value;
+	int key = col + num_cols_ * row;
+	for (int i = 0; i < num_terms_;i++)
+	{
+		terms_[row * num_cols_ + col].value = value;
+	}
+	
 }
 
 float SparseMatrix::GetValue(int row, int col) const // 맨 뒤의 const는 함수 안에서 멤버 변수의 값을 바꾸지 않겠다는 의미
@@ -58,7 +64,17 @@ SparseMatrix SparseMatrix::Transpose()
 	SparseMatrix temp(num_cols_, num_rows_, capacity_); // num_cols_, num_rows_ 순서 주의
 
 	// TODO:
+	temp.num_cols_ = num_rows_;
+	temp.num_rows_ = num_cols_;
+	temp.capacity_ = capacity_;
 
+	for (int i = 0; i < num_rows_;i++)//6
+	{
+		for (int j = 0;j < num_cols_;j++)//4
+		{
+			temp.terms_[i * num_cols_ + j].value = terms_[i * num_cols_ + j].value;
+		}
+	}
 	return temp;
 }
 
