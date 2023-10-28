e79433fd68f0b1a7fb0421581269cb2b318df3c8
sdj3701 <110330701+sdj3701@users.noreply.github.com>
2023-10-28 10:26:41 +0900

    sad

--- hongData/hongData/hongData/RecurPermutations.cpp
+++ hongData/hongData/hongData/RecurPermutations.cpp
@@ -8,6 +8,15 @@ void Swap(char& left, char& right);
 
 void RecurPermutations(char* arr, int left, int right)
 {
+	char car[3];
+	int n = sizeof(car) / sizeof(car[0]);
+
+	for (int i = 0;i < n - 1; i++)
+	{
+		car[i] = arr[i];
+	}
+	Swap(car[0], car[left]);
+
 	if (left == right)
 	{
 		for (int i = 0;i <= right; i++)
@@ -16,6 +25,7 @@ void RecurPermutations(char* arr, int left, int right)
 	}
 	else
 	{
+<<<<<<< Updated upstream
 		for (int i = left; i <= right; i++)
 		{
 			Swap(arr[left], arr[i]);
@@ -25,6 +35,17 @@ void RecurPermutations(char* arr, int left, int right)
 			Swap(arr[left], arr[i]);
 
 		}
+=======
+			for (int i = 1; i < n - 1;i++)
+			{
+				cout << car[i] << " ";
+			}
+			cout << endl;
+			b++;
+			Swap(car[left + b], car[right]);
+		
+		RecurPermutations(arr, left + 1, right);
+>>>>>>> Stashed changes
 	}
 }
 
